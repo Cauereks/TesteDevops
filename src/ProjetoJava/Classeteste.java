46e0310e5ab83ed9fdb4851f3855ac2037843552
Caue Antonio <shandownpacksz13@gmail.com>
2025-08-29 09:03:33 -0300

    Calculadora Java(Soma)

--- src/ProjetoJava/Classeteste.java
+++ src/ProjetoJava/Classeteste.java
@@ -1,9 +1,27 @@
 package ProjetoJava;
 
+import java.util.Scanner;
+
 public class Classeteste {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		/*Scanner scanner = new Scanner(System.in);
+
+        System.out.println("=== Calculadora de Adição ===");
+        System.out.print("Digite o primeiro número: ");
+        double numero1 = scanner.nextDouble();
+
+        System.out.print("Digite o segundo número: ");
+        double numero2 = scanner.nextDouble();
+
+        double resultado = numero1 + numero2;
+
+        System.out.println("Resultado da soma: " + resultado);
+
+        scanner.close();*/
+		
+		
+		
 
 	}
 
